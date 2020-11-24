04d22112e688a417b426ab8ac9bf592734d37f5e
zhanggehui <395510548@qq.com>
2020-11-24 21:46:10 +0800

    update

--- run_lammps.sh
+++ run_lammps.sh
@@ -1,2 +1,18 @@
-lmp -in ../init.in
-source ../make_atomfile.sh
+currentdir=`pwd`
+cd /home/liufeng_pkuhpc/lustre2/zgh/zgh_lmp/lmp_use/
+gitget
+cd $currentdir
+
+rundir=$1
+sub_infile=$2
+#rm -rf $rundir
+if [ ! -d $rundir ]; then
+    mkdir $rundir 
+    cd $rundir
+    lmp -in ../infiles/init.in
+    source ../make_atomfile.sh
+    sbatch /home/liufeng_pkuhpc/lustre2/zgh/zgh_lmp/lmp_use/sub_lmp.sh $sub_infile
+    cd ..
+else
+    echo 'Already exists! Please make sure!'
+fi
