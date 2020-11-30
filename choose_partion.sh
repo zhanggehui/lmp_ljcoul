5969ad698feccc2bfd8162dd127a76b051a40def
zhanggehui <395510548@qq.com>
2020-11-30 11:15:54 +0800

    update

--- choose_partion.sh
+++ choose_partion.sh
@@ -6,8 +6,7 @@ awk '
     /cn_nl/{ cnnl=substr($0,33,2);cnnl=cnnl+0;} 
     /cn-short/{ cnshort=substr($0,33,2);cnshort=cnshort+0;} 
     /cn_long/{ cnlong=substr($0,33,2);cnlong=cnlong+0;} 
-    END
-    { if (cnnl>=cnshort)
+    END { if (cnnl>=cnshort)
       {
       choosen=cnnl
       }
